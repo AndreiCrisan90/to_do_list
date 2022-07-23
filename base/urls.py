d7cf55cdce4e5d4e7ebbc058a8134d82d5b9c330
AndreiCrisan90 <andurapp1990@yahoo.com>
2022-07-23 11:08:45 +0300

    my simple app

---
+++ base/urls.py
@@ -0,0 +1,6 @@
+from . import views
+from django.urls import path
+from .views import TaskList, TaskDetail
+urlpatterns = [path('', TaskList.as_view(), name='tasks'),
+                path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+               ]
